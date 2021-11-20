6a85370a71ec62459f81368a1af68efda293e154
LWJ1608 <2194677100@qq.com>
2021-11-20 09:46:35 +0800

    c#

---
+++ 算数运算符/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿
+namespace 算数运算符
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblShow = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(-134, -61);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // lblShow
+            // 
+            this.lblShow.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblShow.Location = new System.Drawing.Point(42, 26);
+            this.lblShow.Name = "lblShow";
+            this.lblShow.Size = new System.Drawing.Size(709, 391);
+            this.lblShow.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblShow);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "显示4位数中千百十各位数";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblShow;
+    }
+}
+
---
+++ 算数运算符/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 算数运算符
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            int num = 4321;
+            lblShow.Text += "千位数是：" + num / 1000;
+            lblShow.Text += "\n" + "百位数是：" + num / 100 % 10;
+            lblShow.Text += "\n" + "十位数是：" + num / 10 % 10;
+            lblShow.Text += "\n" + "十位数是：" + num% 10;
+        }
+    }
+}
