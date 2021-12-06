4559d631b55ec0667831d8db9052f26cc288f211
LWJ1608 <2194677100@qq.com>
2021-12-06 23:50:25 +0800

    C#

---
+++ 打印菱形/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace 打印菱形
+{
+    class Program
+    {
+        static void Show(int p)
+        {
+            int t= 2 * p - 1;
+            if (p>2)
+            {
+                for (int i = 1; i <= t / 2 + 1; i++) 
+                {
+                    for (int a = 1; a <= t / 2 + 1; a++) 
+                        {
+                            if(a>=t/2-i+2)
+                            {
+                            Console.Write(" *");
+                            }
+                            else 
+                            {
+                                Console.Write(" ");
+                            }
+                        }
+                    Console.WriteLine();
+                }
+
+                for (int i = t / 2; i >=1 ; i--)
+                {
+                    for (int a = 1; a <=t/2+1; a++)
+                    {
+                        if (a >= t / 2 - i + 2)
+                        {
+                            Console.Write(" *");
+                        }
+                        else
+                        {
+                            Console.Write(" ");
+                        }
+                    }
+                    Console.WriteLine();
+                }
+            }
+            else
+            {
+                Console.WriteLine("输入的数不能打印组成一个菱形！");
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("请输入一个数");
+            string a = Console.ReadLine();
+            int n = Convert.ToInt32(a);
+
+/*            Console.WriteLine(n);*/
+            Show(n);
+            /*            Console.WriteLine("Hello World!");*/
+        }
+    }
+}
